07a0aaf020cfaeb4ef00fbf103f54324c4a2c090
aayush0851 <tyagiayush851@gmail.com>
2020-03-17 13:25:59 +0530

    list component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-class Form extends React.Component{
+class App extends React.Component{
   constructor(){
     super();
     this.state={
@@ -17,28 +17,66 @@ class Form extends React.Component{
   }
   submitForm(e) {
     e.preventDefault();
-    console.log(this.state.task)
+    console.log(this.state.task);
     alert('Task added');
+    arr.unshift(this.state.task);
     this.setState({
       task: ''
     })
+    console.log(arr);
   }
   render(){
     return(
-      <form onSubmit={this.submitForm}>
-        <input 
-          type="text"
-          placeholder="Enter the task here"
-          onChange={this.taskWritten}
-          value={this.state.task}
+      <div>
+        {/* <form onSubmit={this.submitForm}>
+          <input 
+            type="text"
+            placeholder="Enter the task here"
+            onChange={this.taskWritten}
+            value={this.state.task}
+            />
+          <input 
+            type="submit" value="Enter"/>
+        </form> */}
+        <Form 
+            onPost={this.submitForm}
+            onEdit={this.taskWritten}
+            data={this.state.value}
           />
-        <input 
-          type="submit" value="Enter"/>
-      </form>
+        <List />
+      </div>
     )
   }
 }
 
-const tasks = [];
+class Form extends React.Component {
+  render() {
+    return(
+      <div>
+          <form onSubmit={this.props.onPost}>
+          <input 
+            type="text"
+            placeholder="Enter the task here"
+            onChange={this.props.onEdit}
+            value={this.props.data}
+            />
+          <input 
+            type="submit" value="Enter"/>
+        </form>
+      </div>
+    )
+  }
+}
 
-export default Form;
+class List extends React.Component {
+  render(){
+    const listItems = arr.map((item)=> <li>{item}</li>);
+    return(
+      <ul>{listItems}</ul>
+    )
+  }
+}
+
+const arr = [];
+
+export default App;
